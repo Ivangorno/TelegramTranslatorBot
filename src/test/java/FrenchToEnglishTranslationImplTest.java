b95e5e4f85cb53b27cad1796f0232b7c6ee96d68
Kulakivsky <ruslankulakivsky@gmail.com>
2023-03-19 15:50:29 +0200

    added junit test logic into the project

--- src/test/java/FrenchToEnglishTranslationImplTest.java
+++ src/test/java/FrenchToEnglishTranslationImplTest.java
@@ -1,21 +1,30 @@
+import com.EnglishToFrenchDictionary;
 import com.FrenchToEnglishTranslationImpl;
 import com.TgDictionaryBot;
+import com.UTF8Validation;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest( classes = EnglishToFrenchDictionary.class)
+@MockBean({
+        UTF8Validation.class,
+        TgDictionaryBot.class
+})
 class FrenchToEnglishTranslationImplTest {
 
-    private TgDictionaryBot tgDictionaryBot = new TgDictionaryBot();
-    FrenchToEnglishTranslationImpl frenchToEnglishTranslation = new FrenchToEnglishTranslationImpl();
-
+    @Autowired
+    private EnglishToFrenchDictionary englishToFrenchDictionary;
 
     @Test
     void frToEngTranslate() {
-       assertEquals("Hello",frenchToEnglishTranslation.translateFrenchToEnglish("Bonjour"));
-    }
-
-    @Test
-    void engToFrTranslate() {
+       assertEquals("Bonjour",englishToFrenchDictionary.getTranslation("Hello"));
     }
 }
